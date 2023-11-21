5ba62d7fb22d793c6c6b9dff1d206bdc8a60ca70
iamrosada <luisrosada2018@gmail.com>
2023-11-21 12:20:10 +0300

    feat: added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
-import { Providers } from '@/Providers';
-import TopNavbar from '@/shared/UI/Navbar/Top';
-import Skill from './shared/UI/Skill/Skill';
-import SpeakComponent, {
-  FooterComponent,
-  OuServiceComponent,
-  UserExperience,
-} from './shared/UI/Speak/Speak';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './routes/router';
 
 function App() {
   return (
-    <Providers>
-      <header>
-        <TopNavbar />
-      </header>
-      <main>
-        <Skill />
-        <SpeakComponent />
-        <OuServiceComponent />
-        <UserExperience />
-      </main>
-      <footer>
-        <FooterComponent />
-      </footer>
-    </Providers>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   );
 }
 
