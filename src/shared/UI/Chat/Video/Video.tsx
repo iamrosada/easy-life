bb1ade0b95574b7e8705c56c64bb693c94c4b63b
iamrosada <luisrosada2018@gmail.com>
2023-12-04 14:39:59 +0300

    feat: created all element inside of host call

--- src/shared/UI/Chat/Video/Video.tsx
+++ src/shared/UI/Chat/Video/Video.tsx
@@ -4,111 +4,159 @@ export const VideoHostCall = () => {
   return (
     <Box className="pt-2">
       <Box className="w-[74vw] h-[700px] mr-4 rounded-[15px] pt-2 bg-white mb-2">
-        <Box>
-          <Box className="flex flex-row justify-between p-1">
-            <Box className="flex w-max-[30px] ">
-              <Text className="text-black mt-[13px] ml-[49px] h-[33px] w-[85px] font-medium text-[20px] leading-[32.67px]">
-                12:23:44
-              </Text>
+        <Box className="flex flex-row justify-between p-1">
+          <Box className="flex w-max-[30px] ">
+            <Text className="text-black mt-[13px] ml-[49px] h-[33px] w-[85px] font-medium text-[20px] leading-[32.67px]">
+              12:23:44
+            </Text>
 
-              <Box className="flex absolute">
-                <svg
-                  width="166"
-                  height="51"
-                  viewBox="0 0 166 51"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <g filter="url(#filter0_b_29_994)">
-                    <rect
-                      width="166"
-                      height="51"
-                      rx="25.5"
-                      fill="#0F0E0E"
-                      fill-opacity="0.3"
-                    />
-                  </g>
-                  <circle
-                    cx="25.5825"
-                    cy="25.5823"
-                    r="15.1406"
-                    fill="white"
-                    fill-opacity="0.63"
-                  />
-                  <circle
-                    cx="25.5823"
-                    cy="25.5823"
-                    r="5.74297"
-                    fill="#EB5757"
-                  />
-                  <defs>
-                    <filter
-                      id="filter0_b_29_994"
-                      x="-15"
-                      y="-15"
-                      width="196"
-                      height="81"
-                      filterUnits="userSpaceOnUse"
-                      color-interpolation-filters="sRGB"
-                    >
-                      <feFlood flood-opacity="0" result="BackgroundImageFix" />
-                      <feGaussianBlur
-                        in="BackgroundImageFix"
-                        stdDeviation="7.5"
-                      />
-                      <feComposite
-                        in2="SourceAlpha"
-                        operator="in"
-                        result="effect1_backgroundBlur_29_994"
-                      />
-                      <feBlend
-                        mode="normal"
-                        in="SourceGraphic"
-                        in2="effect1_backgroundBlur_29_994"
-                        result="shape"
-                      />
-                    </filter>
-                  </defs>
-                </svg>
-              </Box>
-            </Box>
-            <Box className="bg-[#0F0E0E] h-[60px] w-[60px] flex items-center justify-center rounded-full">
+            <Box className="flex absolute">
               <svg
-                width="28"
-                height="28"
-                viewBox="0 0 28 28"
+                width="166"
+                height="51"
+                viewBox="0 0 166 51"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <path
-                  d="M2.3335 10.5V7.58331C2.3335 4.67831 4.6785 2.33331 7.5835 2.33331H10.5002"
-                  stroke="white"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                <g filter="url(#filter0_b_29_994)">
+                  <rect
+                    width="166"
+                    height="51"
+                    rx="25.5"
+                    fill="#0F0E0E"
+                    fill-opacity="0.3"
+                  />
+                </g>
+                <circle
+                  cx="25.5825"
+                  cy="25.5823"
+                  r="15.1406"
+                  fill="white"
+                  fill-opacity="0.63"
                 />
-                <path
-                  d="M17.5 2.33331H20.4167C23.3217 2.33331 25.6667 4.67831 25.6667 7.58331V10.5"
-                  stroke="white"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M25.6665 18.6667V20.4167C25.6665 23.3217 23.3215 25.6667 20.4165 25.6667H18.6665"
-                  stroke="white"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-                <path
-                  d="M10.5002 25.6667H7.5835C4.6785 25.6667 2.3335 23.3217 2.3335 20.4167V17.5"
-                  stroke="white"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
+                <circle cx="25.5823" cy="25.5823" r="5.74297" fill="#EB5757" />
+                <defs>
+                  <filter
+                    id="filter0_b_29_994"
+                    x="-15"
+                    y="-15"
+                    width="196"
+                    height="81"
+                    filterUnits="userSpaceOnUse"
+                    color-interpolation-filters="sRGB"
+                  >
+                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                    <feGaussianBlur
+                      in="BackgroundImageFix"
+                      stdDeviation="7.5"
+                    />
+                    <feComposite
+                      in2="SourceAlpha"
+                      operator="in"
+                      result="effect1_backgroundBlur_29_994"
+                    />
+                    <feBlend
+                      mode="normal"
+                      in="SourceGraphic"
+                      in2="effect1_backgroundBlur_29_994"
+                      result="shape"
+                    />
+                  </filter>
+                </defs>
               </svg>
+            </Box>
+          </Box>
+          <Box className="bg-[#0F0E0E] h-[60px] w-[60px] flex items-center justify-center rounded-full">
+            <svg
+              width="28"
+              height="28"
+              viewBox="0 0 28 28"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M2.3335 10.5V7.58331C2.3335 4.67831 4.6785 2.33331 7.5835 2.33331H10.5002"
+                stroke="white"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M17.5 2.33331H20.4167C23.3217 2.33331 25.6667 4.67831 25.6667 7.58331V10.5"
+                stroke="white"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M25.6665 18.6667V20.4167C25.6665 23.3217 23.3215 25.6667 20.4165 25.6667H18.6665"
+                stroke="white"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M10.5002 25.6667H7.5835C4.6785 25.6667 2.3335 23.3217 2.3335 20.4167V17.5"
+                stroke="white"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </Box>
+        </Box>
+
+        <Box className="h-[60vh] flex  items-center">
+          <Box className="mt-auto">
+            <Box className="flex justify-between items-center p-2 w-[74vw]">
+              <Box className=" flex justify-center items-center bg-black w-[189.8px] h-[49.33px] p-[8.17px 29.4px 8.17px 29.4px] rounded-[89.83px] gap-[16.33px]">
+                Adam Joseph
+              </Box>
+              <Box className="px-4 py-2 bg-[#0F0E0E] h-[60px] w-[60px] flex  rounded-full">
+                <svg
+                  width="38"
+                  height="38"
+                  viewBox="0 0 38 38"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M5 11L5 28"
+                    stroke="white"
+                    stroke-width="2.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M12 13L12 26"
+                    stroke="white"
+                    stroke-width="2.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M19 5.14583V32.8542"
+                    stroke="white"
+                    stroke-width="2.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M26 13V26"
+                    stroke="white"
+                    stroke-width="2.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M33 11L33 28"
+                    stroke="white"
+                    stroke-width="2.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </Box>
             </Box>
           </Box>
         </Box>
