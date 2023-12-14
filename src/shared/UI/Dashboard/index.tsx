a9d32099c5f64d58f211e2ffd7154e3367fc3ed6
iamrosada <luisrosada2018@gmail.com>
2023-12-14 16:36:30 +0300

    feat: building the event

--- src/shared/UI/Dashboard/index.tsx
+++ src/shared/UI/Dashboard/index.tsx
@@ -7,6 +7,14 @@ import {
   TabPanel,
   TabPanels,
   Tabs,
+  Card,
+  Image,
+  Stack,
+  Heading,
+  CardBody,
+  Text,
+  CardFooter,
+  Button,
 } from '@chakra-ui/react';
 import { useToast } from '@chakra-ui/react';
 
@@ -17,7 +25,7 @@ import {
   PlusIcon,
 } from '@heroicons/react/20/solid';
 //import { EllipsisVerticalIcon } from '@heroicons/react/24/outline';
-import { Fragment, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import {
   Dialog,
   RadioGroup,
@@ -104,51 +112,32 @@ export default function Dashboard() {
     </Box>
   );
 }
+// useEffect(() => {
+//   async function fetchData() {
+//     // You can await here
+//     const response = await ClassService.getClassById(
+//       getStudent?.event_id as string
+//     );
+//     setEvents([...events, response]);
+//   }
+//   fetchData();
+// }, [getStudent?.event_id]);
 
-const days = [
-  { date: '2021-12-27' },
-  { date: '2021-12-28' },
-  { date: '2021-12-29' },
-  { date: '2021-12-30' },
-  { date: '2021-12-31' },
-  { date: '2022-01-01', isCurrentMonth: true },
-  { date: '2022-01-02', isCurrentMonth: true },
-  { date: '2022-01-03', isCurrentMonth: true },
-  { date: '2022-01-04', isCurrentMonth: true },
-  { date: '2022-01-05', isCurrentMonth: true },
-  { date: '2022-01-06', isCurrentMonth: true },
-  { date: '2022-01-07', isCurrentMonth: true },
-  { date: '2022-01-08', isCurrentMonth: true },
-  { date: '2022-01-09', isCurrentMonth: true },
-  { date: '2022-01-10', isCurrentMonth: true },
-  { date: '2022-01-11', isCurrentMonth: true },
-  { date: '2022-01-12', isCurrentMonth: true, isToday: true },
-  { date: '2022-01-13', isCurrentMonth: true },
-  { date: '2022-01-14', isCurrentMonth: true },
-  { date: '2022-01-15', isCurrentMonth: true },
-  { date: '2022-01-16', isCurrentMonth: true },
-  { date: '2022-01-17', isCurrentMonth: true },
-  { date: '2022-01-18', isCurrentMonth: true },
-  { date: '2022-01-19', isCurrentMonth: true },
-  { date: '2022-01-20', isCurrentMonth: true },
-  { date: '2022-01-21', isCurrentMonth: true, isSelected: true },
-  { date: '2022-01-22', isCurrentMonth: true },
-  { date: '2022-01-23', isCurrentMonth: true },
-  { date: '2022-01-24', isCurrentMonth: true },
-  { date: '2022-01-25', isCurrentMonth: true },
-  { date: '2022-01-26', isCurrentMonth: true },
-  { date: '2022-01-27', isCurrentMonth: true },
-  { date: '2022-01-28', isCurrentMonth: true },
-  { date: '2022-01-29', isCurrentMonth: true },
-  { date: '2022-01-30', isCurrentMonth: true },
-  { date: '2022-01-31', isCurrentMonth: true },
-  { date: '2022-02-01' },
-  { date: '2022-02-02' },
-  { date: '2022-02-03' },
-  { date: '2022-02-04' },
-  { date: '2022-02-05' },
-  { date: '2022-02-06' },
-];
+// useEffect(() => {
+//   const someId = '19c0dd14-6f81-4622-b645-f04e065bc92f';
+//   async function fetchData() {
+//     // You can await here
+//     const response = await StudentService.getStudentById(someId);
+//     console.log(response, 'res');
+//     const respons = await ClassService.getClassById(
+//       getStudent?.event_id as string
+//     );
+//     setEvents([...events, respons]);
+//     setGetStudent(response);
+//   }
+//   fetchData();
+// }, []);
+
 const meetings = [
   {
     id: 1,
@@ -200,184 +189,160 @@ function classNames(...classes: any) {
   return classes.filter(Boolean).join(' ');
 }
 
+interface EventsI {
+  id: string;
+  title_of_lesson: string;
+  description: string;
+  teacher_id: string;
+  students_ids: string[];
+}
 function Example() {
-  return (
-    <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
-      <div className="md:pr-14">
-        <div className="flex items-center">
-          <h2 className="flex-auto text-sm font-semibold text-gray-900">
-            January 2022
-          </h2>
-          <button
-            type="button"
-            className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
-          >
-            <span className="sr-only">Previous month</span>
-            <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-          </button>
-          <button
-            type="button"
-            className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
-          >
-            <span className="sr-only">Next month</span>
-            <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-          </button>
-        </div>
-        <div className="mt-10 grid grid-cols-7 text-center text-xs leading-6 text-gray-500">
-          <div>M</div>
-          <div>T</div>
-          <div>W</div>
-          <div>T</div>
-          <div>F</div>
-          <div>S</div>
-          <div>S</div>
-        </div>
-        <div className="mt-2 grid grid-cols-7 text-sm">
-          {days.map((day, dayIdx) => (
-            <div
-              key={day.date}
-              className={classNames(
-                dayIdx > 6 && 'border-t border-gray-200',
-                'py-2'
-              )}
-            >
-              <button
-                type="button"
-                className={classNames(
-                  day.isSelected && 'text-white',
-                  !day.isSelected && day.isToday && 'text-indigo-600',
-                  !day.isSelected &&
-                    !day.isToday &&
-                    day.isCurrentMonth &&
-                    'text-gray-900',
-                  !day.isSelected &&
-                    !day.isToday &&
-                    !day.isCurrentMonth &&
-                    'text-gray-400',
-                  day.isSelected && day.isToday && 'bg-indigo-600',
-                  day.isSelected && !day.isToday && 'bg-gray-900',
-                  !day.isSelected && 'hover:bg-gray-200',
-                  (day.isSelected || day.isToday) && 'font-semibold',
-                  'mx-auto flex h-8 w-8 items-center justify-center rounded-full'
-                )}
-              >
-                <time dateTime={day.date}>
-                  {day?.date?.split('-')?.pop().replace(/^0/, '')}
-                </time>
-              </button>
-            </div>
-          ))}
-        </div>
-      </div>
-      <section className="mt-12 md:mt-0 md:pl-14">
-        <h2 className="text-base font-semibold leading-6 text-gray-900">
-          Schedule for <time dateTime="2022-01-21">January 21, 2022</time>
-        </h2>
-        <ol className="mt-4 space-y-1 text-sm leading-6 text-gray-500">
-          {meetings.map(meeting => (
-            <li
-              key={meeting.id}
-              className="group flex items-center space-x-4 rounded-xl px-4 py-2 focus-within:bg-gray-100 hover:bg-gray-100"
-            >
-              <img
-                src={meeting.imageUrl}
-                alt=""
-                className="h-10 w-10 flex-none rounded-full"
-              />
-              <div className="flex-auto">
-                <p className="text-gray-900">{meeting.name}</p>
-                <p className="mt-0.5">
-                  <time dateTime={meeting.startDatetime}>{meeting.start}</time>{' '}
-                  - <time dateTime={meeting.endDatetime}>{meeting.end}</time>
-                </p>
-              </div>
-              <Menu
-                as="div"
-                className="relative opacity-0 focus-within:opacity-100 group-hover:opacity-100"
-              >
-                <div>
-                  <Menu.Button className="-m-2 flex items-center rounded-full p-1.5 text-gray-500 hover:text-gray-600">
-                    <span className="sr-only">Open options</span>
-                    <EllipsisVerticalIcon
-                      className="h-6 w-6"
-                      aria-hidden="true"
-                    />
-                  </Menu.Button>
-                </div>
+  const toast = useToast();
 
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
+  const existingStudents: StudentI[] =
+    JSON.parse(localStorage.getItem('chosenStudents') as any) || [];
+
+  const [students, setStudents] = useState<StudentI[]>(existingStudents);
+  const [getStudent, setGetStudent] = useState<StudentI>({});
+  const [events, setEvents] = useState<EventsI[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<unknown>('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const studentId = '19c0dd14-6f81-4622-b645-f04e065bc92f';
+        const studentResponse = await StudentService.getStudentById(studentId);
+        console.log(studentResponse, 'studentResponse');
+
+        const classResponse = await ClassService.getClassById(
+          studentResponse?.event_id as string
+        );
+
+        setEvents(prevEvents => [...prevEvents, classResponse]);
+        setGetStudent(studentResponse);
+        setLoading(false);
+      } catch (error: any) {
+        console.error('Error fetching data:', error);
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <>
+      {events.map(evnt => (
+        <Card
+          direction={{ base: 'column', sm: 'row' }}
+          overflow="hidden"
+          variant="outline"
+          key={evnt.id}
+          className="mb-8 mx-auto max-w-[1100px] flex flex-row justify-between "
+        >
+          <Stack>
+            <CardBody>
+              <Heading size="md">{evnt.title_of_lesson}</Heading>
+
+              <Text py="2">{evnt.description}</Text>
+            </CardBody>
+
+            <CardFooter>
+              <Button variant="solid" colorScheme="blue">
+                Participar do evento
+              </Button>
+            </CardFooter>
+          </Stack>
+
+          <section className="mt-12 md:mt-0 md:pl-14 mb-[70px]">
+            <h2 className="text-base font-semibold leading-6 text-gray-900 p-8">
+              Estudantes selecionados para a aula!
+            </h2>
+            <ol className="mt-4 space-y-1 text-sm leading-6 text-gray-500">
+              {students.map(student => (
+                <li
+                  key={student.id}
+                  className="group flex items-center space-x-4 rounded-xl px-4 py-2 focus-within:bg-gray-100 hover:bg-gray-100"
                 >
-                  <Menu.Items className="absolute right-0 z-10 mt-2 w-36 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <div className="py-1">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active
-                                ? 'bg-gray-100 text-gray-900'
-                                : 'text-gray-700',
-                              'block px-4 py-2 text-sm'
-                            )}
-                          >
-                            Edit
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active
-                                ? 'bg-gray-100 text-gray-900'
-                                : 'text-gray-700',
-                              'block px-4 py-2 text-sm'
-                            )}
-                          >
-                            Cancel
-                          </a>
-                        )}
-                      </Menu.Item>
+                  <img
+                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                    className="h-10 w-10 flex-none rounded-full"
+                  />
+                  <div className="flex-auto">
+                    <p className="text-gray-900">{student.name}</p>
+                    <p className="mt-0.5">{student.email}</p>
+                  </div>
+                  <Menu
+                    as="div"
+                    className="relative opacity-0 focus-within:opacity-100 group-hover:opacity-100"
+                  >
+                    <div>
+                      <Menu.Button className="-m-2 flex items-center rounded-full p-1.5 text-gray-500 hover:text-gray-600">
+                        <span className="sr-only">Open options</span>
+                        <EllipsisVerticalIcon
+                          className="h-6 w-6"
+                          aria-hidden="true"
+                        />
+                      </Menu.Button>
                     </div>
-                  </Menu.Items>
-                </Transition>
-              </Menu>
-            </li>
-          ))}
-        </ol>
-      </section>
-    </div>
+
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="absolute right-0 z-10 mt-2 w-36 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <div className="py-1">
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className={classNames(
+                                  active
+                                    ? 'bg-gray-100 text-gray-900'
+                                    : 'text-gray-700',
+                                  'block px-4 py-2 text-sm'
+                                )}
+                              >
+                                Edit
+                              </a>
+                            )}
+                          </Menu.Item>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className={classNames(
+                                  active
+                                    ? 'bg-gray-100 text-gray-900'
+                                    : 'text-gray-700',
+                                  'block px-4 py-2 text-sm'
+                                )}
+                              >
+                                Cancel
+                              </a>
+                            )}
+                          </Menu.Item>
+                        </div>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                </li>
+              ))}
+            </ol>
+          </section>
+        </Card>
+      ))}
+    </>
   );
 }
 
-const team = [
-  {
-    name: 'Calvin Hawkins',
-    email: 'calvin.hawkins@example.com',
-    imageUrl:
-      'https://images.unsplash.com/photo-1513910367299-bce8d8a0ebf6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Bessie Richards',
-    email: 'bessie.richards@example.com',
-    imageUrl:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Floyd Black',
-    email: 'floyd.black@example.com',
-    imageUrl:
-      'https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-];
 const settings = [
   {
     name: 'Public access',
@@ -398,6 +363,7 @@ interface StudentI {
   full_name: string;
   course_name: string;
   email: string;
+  event_id: string;
   teachers_ids: string[];
 }
 function CreateClassComponent() {
@@ -420,12 +386,20 @@ function CreateClassComponent() {
     try {
       // Call the API to create the class
       const newClass = await ClassService.createClass({
-        name: className,
+        title_of_lesson: className,
         description: classDescription,
-        // tags: classTags.split(',').map(tag => tag.trim()), // Assuming tags are comma-separated
-        // students: students.map(student => student.email),
+        teacher_id: 'DEUS',
+        students_ids: students.map(student => student.id),
       });
 
+      toast({
+        title: 'Class created successfully',
+        description: 'The Student was notified about this class.',
+        status: 'success',
+        colorScheme: 'purple',
+        duration: 3000,
+        isClosable: true,
+      });
       // Handle the response as needed (e.g., show success message, redirect, etc.)
       console.log('Class created successfully:', newClass);
     } catch (error) {
@@ -664,7 +638,7 @@ function CreateClassComponent() {
               </div>
             </RadioGroup>
 
-            <div>
+            {/* <div>
               <label
                 htmlFor="tags"
                 className="block text-sm font-medium leading-6 text-gray-900"
@@ -677,7 +651,7 @@ function CreateClassComponent() {
                 id="tags"
                 className="mt-2 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-500 sm:text-sm sm:leading-6"
               />
-            </div>
+            </div> */}
 
             <div className="flex justify-end gap-x-3">
               <button
